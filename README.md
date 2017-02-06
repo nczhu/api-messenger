87d94bd313fa8c61c9f450c0c6150eb4c81e8e0a
Nicole Zhu <nicolezhuis@gmail.com>
2017-02-06 18:22:50 +0800

    added better README

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # api-messenger
+
 Integrates API.AI and Facebook Messenger
+=========
+
+A light integration of api.ai and messenger chat platform for Node.js.
+
+## Installation 
+	`npm install api-messenger`
+
+## Usage
+	`TODO`
+
+## Tests
+	`npm test`
+
+## Contributing
+Add unit tests for new functions. Please refactor and feel free to restyle current formatting.
--- test/test.js
+++ test/test.js
@@ -8,6 +8,31 @@ const id = '298372973' // some arbitrary Fb ID
 
 // testing sendToFb
 
+describe('#sendToFb main function', function() {
+   it('should identify & send a single text message', function() {
+   
+   });
+
+   it('should identify & send text messages', function() {
+   
+   });
+
+   it('should identify & send card messages', function() {
+   
+   });
+   it('should identify & send quick replies', function() {
+   
+   });
+
+   it('should identify & send text messages', function() {
+   
+   });
+   it('should identify & send text messages', function() {
+   
+   });
+
+});
+
 // simpler tests for indiv formatting functions
 describe('#text function', function() {
     it('should format simple text', function() {
@@ -21,7 +46,6 @@ describe('#text function', function() {
         	}
         };
         var result = apiFb.text(id, apiJSON);
-        // console.log(JSON.stringify(result));
         expect(result).to.eql(expected);
     });
 
@@ -36,7 +60,6 @@ describe('#text function', function() {
         	}
         };
         var result = apiFb.text(id, apiJSON);
-        // console.log(JSON.stringify(result));
         expect(result).to.eql(expected);
     });
 
@@ -51,11 +74,39 @@ describe('#text function', function() {
         	}
         };
         var result = apiFb.text(id, apiJSON);
-        // console.log(JSON.stringify(result));
         expect(result).to.eql(expected);
     });
 });
 
 describe('#card function', function() {
+   it('should format single card', function() {
    
+   });
+
+   it('should format multiple cards', function() {
+   
+   });
+   it('should format multiple carousells separated by noncard content', function() {
+   
+   });
+
 });
+
+describe('#image function', function() {
+   it('should format images', function() {
+   
+   });
+
+});
+
+describe('#quick_reply function', function() {
+   it('should format quick replies', function() {
+   
+   });
+
+   it('should format quick replies with emoticons', function() {
+   
+   });
+
+});
+
